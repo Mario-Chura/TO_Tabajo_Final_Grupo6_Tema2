6938cd2d64c33cb3b0dd96327d5aa0adf625df0d
Notch986 <dhuanaco@unsa.edu.pe>
2023-12-29 00:23:46 -0500

    base c
    base c

--- estadistica2.h
+++ estadistica2.h
@@ -1,7 +1,9 @@
 #ifndef ESTADISTICA2_H
 #define ESTADISTICA2_H
 
+#include "AVLTree.h"
 #include <QMainWindow>
+#include <vector>
 
 namespace Ui {
 class Estadistica2;
@@ -12,14 +14,18 @@ class Estadistica2 : public QMainWindow
     Q_OBJECT
 
 public:
+    explicit Estadistica2(AVLTree* tree, QWidget *parent = nullptr);
+    explicit Estadistica2(std::vector<std::vector<Registro*>>* DEPARTAMENTO_MUESTRAS, QWidget *parent = nullptr);
     explicit Estadistica2(QWidget *parent = nullptr);
     ~Estadistica2();
 
 private slots:
     void on_menu_clicked();
 
+    void on_carga_clicked();
+
 private:
     Ui::Estadistica2 *ui;
+    std::vector<std::vector<Registro*>>* DEPARTAMENTO_MUESTRAS; // Almacena el puntero al vector
 };
-
 #endif // ESTADISTICA2_H
